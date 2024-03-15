f2b32bc390afc560ea1aad050cede134fb6086b8
Shantanu Lad <shaan.lad@gmail.com>
2024-03-14 23:47:52 -0500

    Fixing Decorator - In progress

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -2,8 +2,8 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
-    const req = context.switchToHttp.getRequest(); // FIX THIS
-    console.log(req.session.userId);
+    // const req = context.switchToHttp.getRequest(); // FIX THIS
+    // console.log(req.session.userId);
     return 'hi there!';
   },
 );
