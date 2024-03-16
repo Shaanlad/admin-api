e95803e9eb805b62e07efa064656f85d207dd077
Shantanu Lad <shaan.lad@gmail.com>
2024-03-15 22:52:01 -0500

    Adding Auth Guard logic

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './user.model';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
